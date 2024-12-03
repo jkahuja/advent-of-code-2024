3b9bb6facf045672768f8e3dc1afc3578b39b707
jkahuja <jasmine.ahuja11@gmail.com>
2024-12-02 19:59:22 -0500

    fix part 2

--- day2/part2.py
+++ day2/part2.py
@@ -1,31 +1,37 @@
+import copy
 import sys
 
 safe_count = 0
 
-for line in sys.stdin:
-    nums = line.split()
-    nums = [int(num) for num in nums]
+def is_safe(nums):
     decreasing = None
-    deviations = 0
     for i, num in enumerate(nums[:-1]):
         if num == nums[i + 1]:
-            deviations += 1
-            continue
+            return False
         if num > nums[i + 1] and decreasing:
-            deviations += 1
-            continue
+            return False
         if num < nums[i + 1] and decreasing is not None and not decreasing:
-            deviations += 1
-            continue
+            return False
         decreasing = num < nums[i + 1]
         diff = abs(num - nums[i + 1])
         if diff == 0 or diff > 3:
-            deviations += 1
-            continue
-    if deviations <= 1:
-:       safe_count += 1
+            return False
+    return True
+
+
+for line in sys.stdin:
+    nums = line.split()
+    nums = [int(num) for num in nums]
+    if is_safe(nums):
+        safe_count += 1
     else:
-        print(deviations, nums)
+        for i in range(0, len(nums)):
+            new_nums = copy.deepcopy(nums)
+            del new_nums[i]
+            if is_safe(new_nums):
+                safe_count += 1
+                break
+            
 
 print(safe_count)
 
