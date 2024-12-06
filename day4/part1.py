19966dc39af9fb962923cd598969ec08e6034bd7
jkahuja <jasmine.ahuja11@gmail.com>
2024-12-05 22:48:32 -0500

    format

--- day4/part1.py
+++ day4/part1.py
@@ -7,23 +7,37 @@ for line in sys.stdin:
 
 total = 0
 
-NEXT_CHAR_MAP = {'M': 'A', 'A': 'S'}
+NEXT_CHAR_MAP = {"M": "A", "A": "S"}
+
 
 def examine_candidate_recursive(i, j, ch, i_change, j_change):
     if i < 0 or i >= len(words) or j < 0 or j >= len(words[0]):
         return 0
     if words[i][j] != ch:
         return 0
-    if ch == 'S' and words[i][j] == ch:
+    if ch == "S" and words[i][j] == ch:
         return 1
-    return examine_candidate_recursive(i + i_change, j + j_change, NEXT_CHAR_MAP[ch], i_change, j_change)
+    return examine_candidate_recursive(
+        i + i_change, j + j_change, NEXT_CHAR_MAP[ch], i_change, j_change
+    )
+
 
 def examine_candidate(i, j, ch):
-    return examine_candidate_recursive(i - 1, j, ch, -1, 0) + examine_candidate_recursive(i + 1, j, ch, 1, 0) + examine_candidate_recursive(i, j - 1, ch, 0, -1) + examine_candidate_recursive(i, j + 1, ch, 0, 1) + examine_candidate_recursive(i - 1, j - 1, ch, -1, -1) + examine_candidate_recursive(i + 1, j + 1, ch, 1, 1) + examine_candidate_recursive(i + 1, j - 1, ch, 1, -1) + examine_candidate_recursive(i - 1, j + 1, ch, -1, 1)
+    return (
+        examine_candidate_recursive(i - 1, j, ch, -1, 0)
+        + examine_candidate_recursive(i + 1, j, ch, 1, 0)
+        + examine_candidate_recursive(i, j - 1, ch, 0, -1)
+        + examine_candidate_recursive(i, j + 1, ch, 0, 1)
+        + examine_candidate_recursive(i - 1, j - 1, ch, -1, -1)
+        + examine_candidate_recursive(i + 1, j + 1, ch, 1, 1)
+        + examine_candidate_recursive(i + 1, j - 1, ch, 1, -1)
+        + examine_candidate_recursive(i - 1, j + 1, ch, -1, 1)
+    )
+
 
 for i in range(0, len(words)):
     for j in range(0, len(words[0])):
-        if words[i][j] == 'X':
-            total += examine_candidate(i, j, 'M')
+        if words[i][j] == "X":
+            total += examine_candidate(i, j, "M")
 
 print(total)
