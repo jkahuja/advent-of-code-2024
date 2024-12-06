826cc09e0452ba405df3c2c7c89ef87e5b9a5787
jkahuja <jasmine.ahuja11@gmail.com>
2024-12-06 15:57:20 -0500

    day 5

---
+++ day5/part1.py
@@ -0,0 +1,36 @@
+import sys
+
+rules = []
+page_arr = []
+
+for line in sys.stdin:
+    if '|' in line:
+        rules.append(line.strip().split('|'))
+    elif line.strip():
+        page_arr.append(line.strip().split(','))
+        #arr = line.split(',')
+        #for i, page in enumerate(arr):
+        #    page_arr[-1][page] = i
+
+total = 0
+
+for rule in rules:
+    first_page = rule[0]
+    second_page = rule[1]
+
+    def does_match(seq):
+        try:
+            first_pos = seq.index(first_page)
+            second_pos = seq.index(second_page)
+        except ValueError:
+            return True
+        return first_pos < second_pos
+    page_arr[:] = [seq for seq in page_arr if does_match(seq)]
+
+total = 0
+for seq in page_arr:
+    total += int(seq[int(len(seq) / 2)])
+
+print(total)
+
+
