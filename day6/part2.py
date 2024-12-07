97c67de2456b4eae2a286ab75c16af14c3ea6492
jkahuja <jasmine.ahuja11@gmail.com>
2024-12-06 19:28:49 -0500

    part 2

---
+++ day6/part2.py
@@ -0,0 +1,58 @@
+import sys
+import copy
+from collections import defaultdict
+
+lab_map = []
+
+
+for line in sys.stdin:
+    lab_map.append([ch for ch in line.strip()])
+
+for i in range(0, len(lab_map)):
+    for j in range(0, len(lab_map[i])):
+        if lab_map[i][j] == "^":
+            cur_i = i
+            cur_j = j
+            break
+
+DIRECTION_FLIP = {(-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0)}
+
+
+n_obstructions = 0
+
+for m in range(0, len(lab_map)):
+    for n in range(0, len(lab_map[i])):
+        if (m == cur_i and n == cur_j) or lab_map[m][n] != '.':
+            continue
+        map_copy = copy.deepcopy(lab_map)
+        map_copy[m][n] = '#'
+        
+        seen_places = defaultdict(int)
+        i = cur_i
+        j = cur_j
+        dir_i = -1
+        dir_j = 0
+
+        print(m, n)
+
+        while True:
+            seen_places[(i, j)] += 1
+            if seen_places[(i, j)] >= 10:
+                n_obstructions += 1
+                break
+            if i <= 0 or i >= len(map_copy) - 1:
+                break
+            if j <= 0 or j >= len(map_copy[0]) - 1:
+                break
+
+            if map_copy[i + dir_i][j + dir_j] == "#":
+                dir_i, dir_j = DIRECTION_FLIP[(dir_i, dir_j)]
+                i += dir_i
+                j += dir_j
+            else:
+                i += dir_i
+                j += dir_j
+
+
+print(n_obstructions)
+
