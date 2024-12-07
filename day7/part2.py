bbefb3a8787b0e6387da10af161af8948ac594ec
jkahuja <jasmine.ahuja11@gmail.com>
2024-12-07 01:04:19 -0500

    part 2

---
+++ day7/part2.py
@@ -0,0 +1,27 @@
+import sys
+
+
+calib_val = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    arr = line.split(':')
+    target = int(arr[0])
+    nums = arr[1].strip().split(' ')
+    nums = [int(num) for num in nums]
+    possible_ops = [] * len(nums)
+    possible_totals = [nums[0] + nums[1], nums[0] * nums[1], int(str(nums[0]) + str(nums[1]))]
+    for i in range(2, len(nums)):
+        len_possible_totals = len(possible_totals)
+        for j in range(0, len_possible_totals):
+            possible_totals.append(possible_totals[j] + nums[i])
+            possible_totals.append(int(str(possible_totals[j]) + str(nums[i])))
+            possible_totals[j] *= nums[i]
+    for possible_total in possible_totals:
+        if possible_total == target:
+            calib_val += target
+            break
+        
+        
+print(calib_val)
+
